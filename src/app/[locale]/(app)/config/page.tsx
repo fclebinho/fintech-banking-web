c078d15636a70ea63cec0897fcd4b320f0c1ca49
Cleber Gomes <fclebinho@gmail.com>
2024-06-06 20:21:32 -0300

    fix: Add config tela

---
+++ src/app/[locale]/(app)/config/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Config: React.FC = () => {
+  return (
+    <div className="container flex flex-col max-w-7xl">
+      <table>
+        <thead>
+          <tr>
+            <th>Variable</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>NEXT_PUBLIC_AUTHORIZE_API_URL</td>
+            <td>{process.env.NEXT_PUBLIC_AUTHORIZE_API_URL}</td>
+          </tr>
+          <tr>
+            <td>NEXT_PUBLIC_TRANSACTION_API_URL</td>
+            <td>{process.env.NEXT_PUBLIC_TRANSACTION_API_URL}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Config;
